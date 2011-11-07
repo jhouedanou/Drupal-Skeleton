70f7dcb3ee197529912eb42b2cb4235f86043409
Michael Raichelson <i.am@michaelraichelson.com>
2011-11-07 16:16:45 -0500

    shuffling things around.

--- templates/blocks/menu-block-wrapper.tpl.php
+++ templates/blocks/menu-block-wrapper.tpl.php
@@ -1,18 +1,23 @@
 <?php
 /**
  * @file
- * Template file used for blocks created with the menu_block module. 
- * 
- * Module Page: http://drupal.org/project/menu_block
- * Documentation: http://drupal.org/node/748022
+ * Default theme implementation to wrap menu blocks.
+ *
+ * Available variables:
+ * - $content: The renderable array containing the menu.
+ * - $classes: A string containing the CSS classes for the DIV tag. Includes:
+ *   menu-block-DELTA, menu-name-NAME, parent-mlid-MLID, and menu-level-LEVEL.
+ * - $classes_array: An array containing each of the CSS classes.
+ *
+ * The following variables are provided for contextual information.
+ * - $delta: (string) The menu_block's block delta.
+ * - $config: An array of the block's configuration settings. Includes
+ *   menu_name, parent_mlid, title_link, admin_title, level, follow, depth,
+ *   expanded, and sort.
+ *
+ * @see template_preprocess_menu_block_wrapper()
  */
 ?>
-
-<nav id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php if ($block->subject){ ?>
-    <header<?php print $title_attributes; ?>><?php print $block->subject ?></header>
-  <?php } ?>
-  <div class="content">
-    <?php print $content; ?>
-  </div>
+<nav id="<?php print $config['menu_name']; ?>" class="<?php print $classes; ?>">
+  <?php print render($content); ?>
 </nav>
