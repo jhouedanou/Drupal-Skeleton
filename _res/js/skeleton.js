6fc2c2b90335aae7315e20de968255707fee8d5d
Michael Raichelson <i.am@michaelraichelson.com>
2011-11-19 18:35:27 -0500

    Validating JS code with JSLint and JSHint. Left auto-config comment code for both in file.

--- _res/js/skeleton.js
+++ _res/js/skeleton.js
@@ -1,28 +1,39 @@
-var Skeleton = this.Skeleton || {};
+/*jslint maxerr: 50, indent: 2 */
+/*global jQuery:true */
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:true, undef:true, curly:true, browser:true, jquery:true, indent:2, maxerr:50 */
+(function () {
+  "use strict";
 
-Skeleton.config = {
-  animspeed : 500,
-  messagingCloserLink : '<span class="closer">x<\/span>'
-};
+  var Skeleton = this.Skeleton || {};
 
-Skeleton.f = {
-  messagingCloser : function (domElement) {
-    (function($){
-      var $jqElement;
-      $jqElement = $(domElement);
-      $jqElement.append(Skeleton.config.messagingCloserLink);
-      $jqElement.find('span.closer').click(function(){
-        $jqElement.slideUp(Skeleton.config.animspeed);
+  Skeleton.config = {
+    animspeed : 500,
+    messagingCloserLink : '<span class="closer">x<\/span>'
+  };
+
+  Skeleton.f = {
+    messagingCloser : function (domElement) {
+      (function ($) {
+        var $jqElement;
+        $jqElement = $(domElement);
+        $jqElement.append(Skeleton.config.messagingCloserLink);
+        $jqElement.find('span.closer').click(function () {
+          $jqElement.slideUp(Skeleton.config.animspeed);
+        });
+      }(jQuery));
+    }
+  };
+
+  Skeleton.init = function () {
+    (function ($) {
+      $('html.no-js').removeClass('no-js').addClass('with-js');
+      $('div.messages').each(function () {
+        Skeleton.f.messagingCloser(this);
       });
     }(jQuery));
-  }
-};
+  };
 
-Skeleton.init = function(){
-  (function($){
-    $('html.no-js').removeClass('no-js').addClass('with-js');
-    $('div.messages').each(function(){ Skeleton.f.messagingCloser(this); });
-  }(jQuery));
-};
-
-jQuery(function(){ Skeleton.init(); });
+  jQuery(function () {
+    Skeleton.init();
+  });
+}());
