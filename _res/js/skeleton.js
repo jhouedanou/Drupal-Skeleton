cee089818fb6c39ee393c9f9c7e1f7a9778d9fbc
Michael Raichelson <i.am@michaelraichelson.com>
2011-11-19 18:48:26 -0500

    added comment headers to describe what LESS/CSS and JS files are used for. added commenting around what different parts of the code do in JS.

--- _res/js/skeleton.js
+++ _res/js/skeleton.js
@@ -1,3 +1,9 @@
+/**
+ * @file
+ * Scripts used by the Skeleton base theme. 
+ * 
+ * @author MRaichelson <mraichelson@navigationarts.com>
+ */
 /*jslint maxerr: 50, indent: 2 */
 /*global jQuery:true */
 /*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:true, undef:true, curly:true, browser:true, jquery:true, indent:2, maxerr:50 */
@@ -6,13 +12,17 @@
 
   var Skeleton = this.Skeleton || {};
 
+  // Skeleton configuration and reused settings.
   Skeleton.config = {
     animspeed : 500,
     messagingCloserLink : '<span class="closer">x<\/span>'
   };
 
+  // Skeleton reusable functions.
   Skeleton.f = {
     messagingCloser : function (domElement) {
+      // This function adds a close element to Drupal's system messaging
+      // elements so that they can be collapsed out of the way.
       (function ($) {
         var $jqElement;
         $jqElement = $(domElement);
@@ -24,15 +34,17 @@
     }
   };
 
+  // Page initialization tasks.
   Skeleton.init = function () {
     (function ($) {
-      $('html.no-js').removeClass('no-js').addClass('with-js');
+      // Apply a close button to all messaging containers.
       $('div.messages').each(function () {
         Skeleton.f.messagingCloser(this);
       });
     }(jQuery));
   };
 
+  // Use jQuery's $(doc).ready() to call the Skeleton page initialization tasks.
   jQuery(function () {
     Skeleton.init();
   });
